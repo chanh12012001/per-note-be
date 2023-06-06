e8f5c2cece2a9b9499774cb8ebe310e5aad66599
Ho Quang <19520875@gm.uit.edu.vn>
2023-06-06 17:00:49 +0700

    update document update

--- routes/document.routes.js
+++ routes/document.routes.js
@@ -2,9 +2,9 @@ const documentController = require('../controllers/document_controller')
 const express = require('express')
 const router = express.Router()
 
-router.post("/uploadDocument", documentController.createNewDocument);
+router.post("/uploadDocument", documentController.uploadDocument);
 
-router.get("/getAllDocuments", documentController.getAllDocuments);
+router.get("/getAllDocuments", documentController.getAllDocument);
 
 router.delete("/deleteDocument/:id", documentController.deleteDocument);
 
