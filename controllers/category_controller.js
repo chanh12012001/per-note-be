1d98c9f3997064f22e82c384fcb7ebb738f63e3a
Ho Quang <19520875@gm.uit.edu.vn>
2023-06-15 23:58:17 +0700

    update category task

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -1,4 +1,4 @@
-const categoryService = require('../services/category.services')
+const categoryService = require('../services/categories.services')
 
 var functions = {
     createNewCategory: (req, res, next) => {
