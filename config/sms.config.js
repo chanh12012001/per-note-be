8e93cb720afe3cea6a01c6a6225f94461860bfb1
Phạm Văn Chánh <73011753+chanh12012001@users.noreply.github.com>
2023-07-17 21:44:03 +0700

    Update sms.config.js

--- config/sms.config.js
+++ config/sms.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     ACCOUNT_SID: "AC2e6f71651c15ff91345267975b462fde",
-    AUTH_TOKEN : "67105dae8fa7923ce70a2e3a70552cdd",
+    AUTH_TOKEN : "b5133dc4f434a63ef791360271e0c658",
 }
